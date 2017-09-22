4f0844ee6be1dd58357b602c77a62a0e44f30789
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2017-09-22 11:39:19 +0200

    Prepare initial share

--- rules/es2015.js
+++ rules/es2015.js
@@ -11,7 +11,7 @@ module.exports = {
     "global-require": 0,
     "no-unused-vars": ["error", { "vars": "all", "args": "none" }],
     "no-use-before-define": 0,
-    "prefer-template": 0,
+    "prefer-destructuring": 0,
     "prefer-const": [2, {
       "destructuring": "any",
       "ignoreReadBeforeAssign": true
